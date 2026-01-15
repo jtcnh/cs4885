9a6d29c100146f304f1e14e8a3749310886858aa
Jack Chambers <jtcroxley@gmail.com>
2026-01-15 08:51:57 -0800

    Assignment 1 Complete

---
+++ asmt1/main.py
@@ -0,0 +1,23 @@
+import cv2
+import sys
+
+
+# Your Python Version (5 points)
+# https://stackoverflow.com/questions/1252163/printing-python-version-in-output
+print("Python Version:", sys.version)
+
+# Your OpenCV version (5 points)
+print("OpenCV Version:", cv2.getVersionString())
+
+# Your first and last name (5 points)
+print("Name: Jack Chambers")
+
+# Your year in school (5 points)
+print("Year: Senior, Second Semester")
+
+# Your major (5 points)
+print("Major: Computer Engineering")
+
+# Your goals for the class (5 points)
+print("Goals: Learn some cool AI tools, learn about image processing")
+
