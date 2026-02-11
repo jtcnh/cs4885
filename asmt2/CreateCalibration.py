e4f02c4540694e6eb84fd4a9bab4f17797671733
Jack Chambers <jtcroxley@gmail.com>
2026-02-11 15:39:43 -0800

    moved create calibration to seperate file

---
+++ asmt2/CreateCalibration.py
@@ -0,0 +1,99 @@
+"""
+Code Along 1
+1/27/2026
+
+Calibration from Chessboard
+Updated 2/11/2026 to write to file
+
+"""
+import cv2 as cv
+import numpy as np
+import json
+
+
+# Variables
+nValidImages = 0
+rowCorners = 9
+columnCorners = 6
+objPtsArray = []
+imgPtsArray = []
+patternSize = (rowCorners, columnCorners)
+key = ord("k")
+webcam = cv.VideoCapture(0)
+#squareSize = 22
+
+# refine stopping criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TermCriteria_MAX_ITER, 20, 0.001)
+
+# create the grid of corresponding object points
+objectPoints = np.zeros((rowCorners*columnCorners, 3), np.float32)
+objectPoints[:,:2] = np.mgrid[0:rowCorners, 0:columnCorners].T.reshape(-1,2)
+
+# create a calibration function
+def CreateCalibration():
+    # get valid images
+    while (nValidImages < 10) and (key != 27):
+        img = webcam.read()
+        raw = img[1].copy()
+
+        # grayscale
+        gray = cv.cvtColor(raw, cv.COLOR_BGR2GRAY)
+
+        # find chessboard pattern
+        ret, corners = cv.findChessboardCorners(gray, patternSize, None)
+        # print(ret)
+
+        # if we found chesscorners
+        if ret == True:
+            # increase valid image count
+            nValidImages += 1
+
+            # append obj points to arr
+            objPtsArray.append(objectPoints)
+
+            # refine the corners
+            corners2 = cv.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+
+            # append to imgpts array
+            imgPtsArray.append(corners2)
+
+            # draw corners to give visual feedback
+            cv.drawChessboardCorners(raw, patternSize, corners2, ret)
+
+            # save this calibration image to file
+            imgPath = f"./exportedImages/image{nValidImages}.jpg"
+            cv.imwrite(imgPath, raw)
+
+            # show image
+            cv.imshow("Image", raw)
+            key = cv.waitKey(50)
+        else:
+            cv.imshow("Image", raw)
+            key = cv.waitKey(10)
+
+
+    # Save calibration to file
+    ret, iMtx, distort, rotationVectors, translationVectors = cv.calibrateCamera(
+        objPtsArray, 
+        imgPtsArray, 
+        gray.shape[::-1], 
+        None, 
+        None
+    )
+
+    # convert vector tuple to array
+    rotationVectorArray = [rv.tolist() for rv in rotationVectors]
+    translationVectorArray = [tv.tolist() for tv in translationVectors]
+
+    # dict for export
+    exportedCalibration = {
+        "float" : ret,
+        "intrinsic_matrix" : iMtx.tolist(), # need to convert to list for json serialize
+        "distortion_coefs" : distort.tolist(),
+        "rotation" : rotationVectorArray,
+        "translate" : translationVectorArray
+    }
+
+    # export with file ptrs
+    with open("./camera_calibration.json", "w") as calibrationFilePtr:
+        json.dump(exportedCalibration, calibrationFilePtr)
