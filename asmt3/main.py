5fbbe9f5f08a248cbd863ea5c148cdaa3379e428
Jack Chambers <jtcroxley@gmail.com>
2026-02-23 13:30:29 -0800

    asmt3 work

--- asmt3/main.py
+++ asmt3/main.py
@@ -10,12 +10,70 @@ import numpy as np
 
 """
 Process:
-[ ] get webcam image
-[ ] grayscale image
-[ ] background subtraction
+[x] get webcam image
+[x] grayscale image
+[x] background subtraction
 [ ] thresholding?
 [ ] foreground / contour detection
 [ ] add boxes to base image
 """
 
+# globals
+bgsub = cv.createBackgroundSubtractorMOG2()
 webcam = cv.VideoCapture(0)
+key = ord('r')
+
+## videoframe loop
+# breaks when 'ESC' key is pressed (27)
+while (key != 27):
+    # get webcam image
+    img_raw = webcam.read()[1]
+    img = img_raw.copy()
+
+    ## grayscale webcam image
+    grayscale = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    #grayscale = cv.convertScaleAbs(grayscale, alpha=0.25, beta=1)
+    #grayscale = cv.GaussianBlur(grayscale, (21, 21), 0)
+
+    ## background subtraction
+    # refrence: https://docs.opencv.org/3.4/d8/d38/tutorial_bgsegm_bg_subtraction.html
+    foreground = bgsub.apply(img)
+
+    ## create a mask of foreground regions
+    # threshold background 
+    ret, threshMask = cv.threshold(foreground, 127, 255, cv.THRESH_BINARY)
+
+    # TEMP
+    # reference for erosion / dilation
+    # https://medium.com/@siromermer/detecting-and-tracking-moving-objects-with-background-subtractors-using-opencv-f2ff7f94586f
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3,3))
+    #cv.MORPH_OPEN
+    erodedMask = cv.dilate(threshMask, kernel, iterations=2)
+
+    ## locate contours
+    # reference: https://docs.opencv.org/3.4/d4/d73/tutorial_py_contours_begin.html
+    contours, heirarchy = cv.findContours(erodedMask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    ## filter out tiny contours
+    contourSizeFilter = 2500
+    drawnContours = [cnt for cnt in contours if cv.contourArea(cnt) > contourSizeFilter]
+
+    ## draw contours on base image (B,G,R)
+    #cv.drawContours(img_raw, drawnContours, -1, (0,255,0), 3)
+
+    ## draw bounding boxes on motion objects
+    output = img_raw.copy()
+    for contour in drawnContours:
+        x,y,w,h = cv.boundingRect(contour)
+        output = cv.rectangle(output, (x,y), (x+w, y+h), (0,255,0), 3)
+
+
+    ## display raw image with bounding boxes
+    cv.imshow('image', output)
+    cv.imshow('other', erodedMask)
+    key = cv.waitKey(1)
+
+
+# cleanup opencv
+webcam.release()
+cv.destroyAllWindows()
