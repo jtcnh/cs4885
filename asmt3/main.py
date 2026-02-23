4ad49904870430a86ae7d65c3baa56932475c785
Jack Chambers <jtcroxley@gmail.com>
2026-02-23 10:44:53 -0800

    assignment 3 started

---
+++ asmt3/main.py
@@ -0,0 +1,21 @@
+# Jack Chambers
+# Assignment 3
+# Motion Detector
+# 3/5/2026
+
+
+import cv2 as cv
+import numpy as np
+
+
+"""
+Process:
+[ ] get webcam image
+[ ] grayscale image
+[ ] background subtraction
+[ ] thresholding?
+[ ] foreground / contour detection
+[ ] add boxes to base image
+"""
+
+webcam = cv.VideoCapture(0)
